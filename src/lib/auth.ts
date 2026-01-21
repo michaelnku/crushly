430fe0bb7883e6636c284d699dc6252c0e6f2303
michael <nkumichael1@gmail.com>
2026-01-21 16:39:43 +0100

    login form

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,18 @@
+// lib/auth.ts BetterAuth instance
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
 export const auth = betterAuth({
-  database: prisma,
+  adapter: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
 
   emailAndPassword: {
     enabled: true,
   },
+
+  trustedOrigins: ["http://localhost:3001"],
 
   socialProviders: {
     google: {
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,20 @@
-//import { PrismaClient } from "@/generated/prisma";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
 };
 
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["error", "warn"],
+    adapter: new PrismaPg({
+      connectionString: process.env.DATABASE_URL!,
+    }),
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
