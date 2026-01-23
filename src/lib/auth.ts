556d35549b3c5a4aaac6eaf39bc2fefcc52e9ad1
michael <nkumichael1@gmail.com>
2026-01-23 23:55:47 +0100

    login form

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,31 +1,3 @@
-// lib/auth.ts BetterAuth instance
-// import { betterAuth } from "better-auth";
-// import { prismaAdapter } from "better-auth/adapters/prisma";
-// import { prismaAuth } from "./prisma-auth";
-
-// export const auth = betterAuth({
-//   adapter: prismaAdapter(prismaAuth, {
-//     provider: "postgresql",
-//   }),
-
-//   emailAndPassword: {
-//     enabled: true,
-//   },
-
-//   trustedOrigins: ["http://localhost:3001"],
-
-//   socialProviders: {
-//     google: {
-//       clientId: process.env.GOOGLE_CLIENT_ID!,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-//     },
-//   },
-
-//   session: {
-//     expiresIn: 60 * 60 * 24 * 7,
-//   },
-// });
-
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/prisma";
